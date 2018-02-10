272b3badb51c1770e1ea1e83e64cd7f407fec20f
Dorsey Dick <dorseydick@gmail.com>
2018-02-10 11:03:06 -0500

    Add selction reducer

--- src/reducers/SelectionReducer.js
+++ src/reducers/SelectionReducer.js
@@ -1,5 +1,9 @@
 //state is previous state from last time this reducer was executed
-export default (state, action) => {
-  console.log(action);
-  return null;
+export default (state = null, action) => {
+  switch (action.type) {
+    case 'select_library':
+      return action.payload;
+    default:
+      return state;
+  }
 };
