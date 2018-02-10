fd87cf0af826bf17fbc8a148a2a593edce52a5c0
Dorsey Dick <dorseydick@gmail.com>
2018-02-10 10:03:06 -0500

    Add the action creator

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 
 class ListItem extends Component {
 
   render() {
+    console.log(this.props);
     const { titleStyle } = styles;
     return (
       <CardSection>
@@ -22,4 +25,6 @@ const styles = {
   }
 };
 
-export default ListItem;
+//called with mapStateToProps, then bind action creators to component
+//no map state to props so we pass null
+export default connect(null, actions)(ListItem);
